248d3f274d111bd625dd86922f583c44b1dc4bd2
cgodev <carlosgomezrestrepo@outlook.com>
2022-09-07 16:17:56 -0500

    Initial project structure

---
+++ src/components/App.js
@@ -0,0 +1,8 @@
+//TODO: Playlist y buscador de canciones.
+const App = () => {
+    return <div className="container">
+        <h1 className="text-muted">Reactive Music</h1>
+    </div>
+}
+
+export default App;
